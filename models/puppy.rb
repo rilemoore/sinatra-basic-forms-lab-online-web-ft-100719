cb09eb30de4924e5d6ffb4a58cb6b74457b5c579
Riley Moore <rilemoore@gmail.com>
2019-11-25 21:33:08 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,15 +1,11 @@
 class Puppy
   attr_accessor :name, :breed, :age
   
-  @@all = []
   
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
   end
-  
-  def self.all
-    @@all
-  end
+
 end
